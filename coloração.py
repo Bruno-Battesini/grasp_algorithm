9c9568921382d3ff306ad0ae506b35c577c49d03
Matheus <mfkovaleski@inf.ufrgs.br>
2020-11-15 17:31:08 -0300

    load instance

--- coloração.py
+++ coloração.py
@@ -1,27 +1,27 @@
 print("Digite o nome do arquivo")
 fileName = input()
 
-file = open('r', fileName)
+file = open(fileName,'r')
 
-text = file.read()
+text = file.readlines()
 
-temp = str()
+temp = []
 
 graph = {}
 
-vN = int()
-eN = int()
+vN = int() ## vertex number
+eN = int() ## edge number
 
 for line in text:
     if line[0] == 'p':
         temp = line.split(' ')
-        temp = temp.remove('p')
-        temp = temp.remove("edge")
+        temp.remove('p')
+        temp.remove('edge')
         vN = int(temp[0])
         eN = int(temp[1])
     elif line[0] == 'e':
         temp = line.split(' ')
-        temp = temp.remove('e')
+        temp.remove('e')
         v0 = int(temp[0])
         v1 = int(temp[1])
         graph[v0] = v1
