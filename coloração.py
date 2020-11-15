e7ab57a474df4d9ba685307285912d4a9a0b35ac
hcpacheco <hchavespacheco@gmail.com>
2020-11-15 19:08:15 -0300

    Adding find_solution

--- coloração.py
+++ coloração.py
@@ -1,32 +1,42 @@
-print("Digite o nome do arquivo")
-fileName = input()
 
-file = open(fileName,'r')
+def get_graph():
+    print("Digite o nome do arquivo")
+    fileName = input()
 
-text = file.readlines()
+    file = open(fileName,'r')
 
-temp = []
+    text = file.readlines()
 
-graph = {}
+    temp = []
 
-vN = int() ## vertex number
-eN = int() ## edge number
+    graph = {}
 
-for line in text:
-    if line[0] == 'p':
-        temp = line.split(' ')
-        temp.remove('p')
-        temp.remove('edge')
-        vN = int(temp[0])
-        eN = int(temp[1])
-    elif line[0] == 'e':
-        temp = line.split(' ')
-        temp.remove('e')
-        v0 = int(temp[0])
-        v1 = int(temp[1])
-        if v0 in graph:
-            graph[v0].append(v1)
-        else:
-            graph[v0] = [v1]
+    vN = int() ## vertex number
+    eN = int() ## edge number
+
+    for line in text:
+        if line[0] == 'p':
+            temp = line.split(' ')
+            temp.remove('p')
+            temp.remove('edge')
+            vN = int(temp[0])
+            eN = int(temp[1])
+        elif line[0] == 'e':
+            temp = line.split(' ')
+            temp.remove('e')
+            v0 = int(temp[0])
+            v1 = int(temp[1])
+            if v0 in graph:
+                graph[v0].append(v1)
+            else:
+                graph[v0] = [v1]
+
+            if v1 in graph:
+                graph[v1].append(v0)
+            else:
+                graph[v1] = [v0]
+    
+
+    return graph
 
 # print(graph)    
--- grasp.py
+++ grasp.py
@@ -1,11 +1,54 @@
 import random
+import coloração
 
 
-def random_generator(seed, lower_bound, higher_bound):
-    random.seed(seed)
-    return random.randint(lower_bound, higher_bound)
+# def random_generator(seed, lower_bound, higher_bound):
+#     random.seed(seed)
+#     return random.randint(lower_bound, higher_bound)
 
 
-def grasp(alpha, iterations):
-    for n in range(iterations):
-        pass
+# def grasp(alpha, iterations):
+#     for n in range(iterations):
+#         pass
+
+def vertex_to_color(neighbors, color):
+    for n in neighbors:
+        for vertex in color:
+            if(n==vertex):
+                return False
+    return True
+
+def sort_by_degree(x):
+    sorted_graph = {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
+    return sorted_graph.keys()
+
+def find_solution(graph):
+    #sort graph by value length
+    sorted_by_degree = sort_by_degree(graph)
+
+    #generate perturbation
+
+    colors = {}
+
+    for i in sorted_by_degree:
+
+        colored=False
+        for color in colors.keys():
+            if( vertex_to_color(graph[i], colors[color]) ):
+                colors[color].append(i)
+                colored=True
+        if(colored==False):
+            colors_size = len(colors)
+            colors[colors_size] = [i]
+
+    print(colors)
+    return colors
+
+
+def main():
+    graph = coloração.get_graph()
+
+    find_solution(graph)
+
+if __name__ == "__main__":
+    main()
