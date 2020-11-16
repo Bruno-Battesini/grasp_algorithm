22b664bc6d53bbf2f90aa6001a8785943bd3832b
hcpacheco <hchavespacheco@gmail.com>
2020-11-16 01:03:06 -0300

    Adding grasp

--- coloração.py
+++ coloração.py
@@ -1,7 +1,8 @@
 
 def get_graph():
-    print("Digite o nome do arquivo")
-    fileName = input()
+    # print("Digite o nome do arquivo")
+    # fileName = input()
+    fileName = "queen6_6.col"
 
     file = open(fileName,'r')
 
--- grasp.py
+++ grasp.py
@@ -1,6 +1,7 @@
 import random
 import coloração
 import math
+import copy
 
 # def random_generator(seed, lower_bound, higher_bound):
 #     random.seed(seed)
@@ -32,7 +33,7 @@ def find_solution(graph):
     sorted_by_degree = list(sort_by_degree(graph))
 
     #generate perturbation in 10% of the element
-    perturbate_times = math.floor(len(sorted_by_degree)*0.1)
+    perturbate_times = math.floor(len(sorted_by_degree)*0.3)
     for i in range(perturbate_times):
         random_swap(sorted_by_degree)
 
@@ -45,6 +46,7 @@ def find_solution(graph):
             if( has_no_neighbors_in_color(graph[i], colors[color]) ):
                 colors[color].append(i)
                 colored=True
+                break
         if(colored==False):
             colors_size = len(colors)
             colors[colors_size] = [i]
@@ -66,14 +68,25 @@ def is_solution_valid(graph, colors):
     return True
 
 def improved_solution(colors, current_solution):
-    if(len(current_solution) < len(colors)):
+    if(len(current_solution) > len(colors)):
         return True
     return False
+
+def clear_dict(colors):
+    new_dict = {}
+    index = 0
+
+    for color in colors.keys():
+        if(len(colors[color]) > 0):
+            new_dict[index] = colors[color]
+            index += 1
+
+    return new_dict
 
 def generate_new_solution(graph, colors):
     n_vertex = len(graph.keys())
     
-    randomize_n = math.floor(n_vertex*0.1)
+    randomize_n = math.floor(n_vertex*0.15)
 
     random_vertexes = []
 
@@ -86,19 +99,27 @@ def generate_new_solution(graph, colors):
         random_vertex_value = colors[random_color][random_vertex]
 
         del colors[random_color][random_vertex]
+        colors = clear_dict(colors)
+
+
         random_vertexes.append(random_vertex_value)
 
-        # print(random_color, random_vertex, random_vertex_value)
 
     random.shuffle(random_vertexes)
+
 
     # add elements to solution
     for i in random_vertexes:
         colored=False
-        for color in colors.keys():
+        sorted_colors = list(sort_by_degree(colors))
+
+        for color in sorted_colors:
             if( has_no_neighbors_in_color(graph[i], colors[color]) ):
+                # print("adicionando " + str(i) + " em " + str(color))
                 colors[color].append(i)
                 colored=True
+                break
+
         if(colored==False):
             colors_size = len(colors)
             colors[colors_size] = [i]
@@ -106,29 +127,57 @@ def generate_new_solution(graph, colors):
     return colors
 
 def hill_climbing(graph, colors):
-    current_solution = colors
+    current_solution = copy.deepcopy(colors)
+    j=0
 
-    while(not improved_solution(colors,current_solution)):
+    while(not improved_solution(colors, current_solution) and j<100):
 
-        neighborhood_size = 10
+        neighborhood_size = 15
         for i in range(0,neighborhood_size):
-            new_solution = generate_new_solution(graph, colors)
+
+            copy_current_solution = copy.deepcopy(current_solution)
+
+            new_solution = generate_new_solution(graph, copy_current_solution)
+
 
             if(improved_solution(new_solution, current_solution)):
-                current_solution = new_solution
+                current_solution = copy.deepcopy(new_solution)
+        j+=1
+        # print(len(current_solution))
+        # print(j)
 
     return current_solution            
 
 
+def grasp(graph):
+
+    current_solution = {}
+
+    for loop in range(0,100):
+
+        colors = find_solution(graph)
+
+        if(len(current_solution)==0):
+            current_solution=copy.deepcopy(colors)
+
+        current_solution_copy = copy.deepcopy(colors)
+        new_solution = hill_climbing(graph, current_solution_copy)
+
+        if(improved_solution(new_solution, current_solution)):
+            print("atualizado")
+            current_solution = copy.deepcopy(new_solution)
+
+        print("best so far = " + str(len(current_solution)))
+
+    return current_solution
 
 def main():
     graph = coloração.get_graph()
 
-    colors = find_solution(graph)
-
-    colors = hill_climbing(graph,colors)
+    colors = grasp(graph)
 
     print(colors)
+    print(is_solution_valid(graph, colors))
 
 if __name__ == "__main__":
     main()
